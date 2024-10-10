7bafd90431127be039f244ee3831409323a4b639
Jansey Scofield <janscofield10@gmail.com>
2024-10-10 20:57:51 -0300

    Adicionando funcoes de mostrar data formatada e validar a data

--- Data.h
+++ Data.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #ifndef DATA
 #define DATA
 
@@ -6,5 +7,48 @@ typedef struct{
     int mes;
     int ano;
 } Data;
+
+void mostrarDataFormatada(Data data);
+int validarDia(Data data);
+int validarData(Data data);
+
+void mostrarDataFormatada(Data data){
+    printf("%d/%d/%d", data.dia, data.mes, data.ano);
+}
+
+int validarDia(Data data){
+    int diaValido = 1;
+
+    if(data.dia <= 0 || data.dia > 31){
+        diaValido = 0;
+    }
+    else{
+        if(data.dia == 30 && data.mes == 2){
+            if(data.ano % 4 == 0){
+                if(data.ano % 100 == 0 && !(data.ano % 400 == 0)){
+                    diaValido = 0;
+                }
+            }
+            else{
+                diaValido = 0;
+            }
+        }
+        else if(data.dia == 31 && !(data.mes == 1 || data.mes == 3 || data.mes == 5 || data.mes == 7 || data.mes == 8 || data.mes == 10 || data.mes == 12)){
+            diaValido = 0;
+        }
+    }
+
+    return diaValido;
+}
+
+int validarData(Data data){
+    if(data.mes <= 0 || data.mes > 12){
+        return 0;
+    }
+    else if(!validarDia(data)){
+        return 0;
+    }
+    return 1;
+}
 
 #endif
