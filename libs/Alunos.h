f8ee05042ef6530e171034089051ecd7c20de0fc
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-10-17 18:42:19 -0300

    Adicionando menu de alunos e validando a quantidade de alunos na função de listar alunos

--- libs/Alunos.h
+++ libs/Alunos.h
@@ -2,10 +2,11 @@
 #define ALUNOS
 #include <stdio.h>
 #include "Pessoa.h"
-#define TAM_LISTA_ALUNOS 1000
+
 
 void cadastrarAluno();
 void listarAlunosMatriculados();
+void menuAlunos(Pessoa *alunos, int *qtdAlunos);
 
 Pessoa buscarAlunoPorCPF(Pessoa *alunos, int qtdAlunos, char cpf[12]){
     int iCont, jCont;
@@ -33,6 +34,33 @@ Pessoa buscarAlunoPorCPF(Pessoa *alunos, int qtdAlunos, char cpf[12]){
     return null;
 }
 
+void menuAlunos(Pessoa *alunos, int *qtdAlunos){
+    int opcao;
+
+    printf("    Alunos  /n");
+    printf("O que deseja fazer?/n");
+    printf("1 - Cadastrar alunos/n2 - Listar alunos matriculados/n3 - Editar aluno por matrícula/n4 - Deletar aluno por matrícula/n5 - Sair");
+    scanf("%d", &opcao);
+    switch (opcao){
+        case 1:
+            cadastrarAluno(alunos, qtdAlunos);
+            break;
+        case 2:
+            listarAlunosMatriculados(alunos, *qtdAlunos);
+            break;
+        case 3:
+            // Fazer posteriomente
+            break;   
+        case 4:
+            // Fazer posteriomente
+            break;
+        case 5:
+            break;
+        default:
+            printf("Valor invalido.");   
+    }
+}
+
 void cadastrarAluno(Pessoa *alunos, int *qtdAlunos){
     Pessoa novoAluno;
     novoAluno = cadastrarPessoa(*qtdAlunos + 1);
@@ -50,8 +78,13 @@ void cadastrarAluno(Pessoa *alunos, int *qtdAlunos){
 }
 
 void listarAlunosMatriculados(Pessoa *alunos, int qtdAlunos){
-    printf("Todos os alunos matriculados:\n");
-    mostrarPessoas(alunos, qtdAlunos);
+    if(qtdAlunos == 0 ){
+        printf("Nao ha alunos cadastrados.");
+    }
+    else{
+        printf("Todos os alunos matriculados:\n");
+        mostrarPessoas(alunos, qtdAlunos);
+    }
 }
 
 #endif
