9f7b6c0690d0642596ba4e852b95a41003cc69f2
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-10-17 18:41:18 -0300

    Adicionando lista de alunos no main

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include "libs/Pessoa.h"
+#include "libs/Alunos.h"
+#define TAM_LISTAS 1000
 
-void main(){
-    printf("Hello World!");
+int main(){
+    Pessoa alunos[TAM_LISTAS];
+    return 1;
 }
