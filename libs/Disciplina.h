a1b3c6750503174ce10cdad74a386b8496f2627f
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-10-18 18:36:07 -0300

    Começando a função de cadastrar disciplina

--- libs/Disciplina.h
+++ libs/Disciplina.h
@@ -2,6 +2,8 @@
 #include "Pessoa.h"
 #define TAM_LISTA_ALUNOS 50
 
+void cadastrarDisciplina(Disciplina *disciplinas, int *qtdDisciplinas, Pessoa *pessoas, int qtdPessoas);
+
 typedef struct{
 	int codigo;
 	char nome[125];
@@ -10,3 +12,22 @@ typedef struct{
 	Pessoa professor;
 	Pessoa alunos[TAM_LISTA_ALUNOS];	
 }Disciplina;
+
+void cadastrarDisciplina(Disciplina *disciplinas, int *qtdDisciplinas, Pessoa *pessoas, int qtdPessoas){
+	Disciplina novaDisciplina;
+	char cpf[TAM_CPF];
+	printf("Informe o nome:\n");
+	fgets(novaDisciplina.nome, 125, stdin);
+	printf("Digite a turma:\n");
+	scanf("%d", &novaDisciplina.turma);
+	printf("Digite o semestre da disciplina:\n");
+	scanf("%d", &novaDisciplina.semestre);
+	printf("Digite o CPF do professor da matéria: ");
+	fgets(cpf, TAM_CPF, stdin);
+	Pessoa professor = buscarPessoaPorCPF(pessoas, qtdPessoas, cpf);
+	while(!professor.ativa){
+		printf("Esse professor esta inativo ou nao existe. Digite outro CPF\n");
+		fgets(cpf, TAM_CPF, stdin);
+		professor = buscarPessoaPorCPF(pessoas, qtdPessoas, cpf);
+	}
+}
