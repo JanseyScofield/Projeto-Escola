1ff21e0719f2bfe974c9dabdc067ea4096ccf97d
Cauã Will Sacramento <cauawillqssacramento@gmail.com>
2024-10-20 19:26:34 -0300

    Subindo main (teste em seu pc)

--- main.c
+++ main.c
@@ -1,12 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "libs/Pessoa.h"
-#include "libs/Alunos.h"
-#define TAM_LISTAS 1000
+#include "libs/utils.h"
+#include "libs/Modulo_Professor.h"
+
+void menuInicial();
 
 int main(){
-    int c = TAM_CPF;
-    printf("%d", c);
+     int qtdAlunos = 0, qtdProfessores = 0, qtdDisciplinas = 0;
+     int sair = 0;
+     int opcao;
 
-    Pessoa alunos[TAM_LISTAS];
-    return 1;
+     Pessoa lstAlunos[TAM_LISTAS];
+     Pessoa lstProfessores[TAM_LISTAS];
+
+
+     while(!sair) {
+          system("clear||cls");
+          printf("Projeto Escola\n");
+          menuInicial();
+          
+          scanf("%d", &opcao);
+          limparBuffer();
+
+          switch(opcao) {
+               case 0: {
+                    system("clear||cls");
+                    sair = 1;
+                    break;
+               }
+               case 2: {
+                    int sairProfessor = 0;
+                    system("clear||cls");
+                    ModuloProfessor(lstProfessores, &qtdProfessores);
+
+                    break;
+               }
+               default: {
+                    system("clear||cls");
+                    printf("Entrada invalida. Aperte um botao para continuar.\n");
+                    getc(stdin);
+               }
+          }
+     }
+}
+
+void menuInicial() {
+    printf("0 - Sair\n");
+    printf("1 - Aluno\n");
+    printf("2 - Professor\n");
+    printf("3 - Disciplina\n");
 }
