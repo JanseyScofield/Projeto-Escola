60a7ef671375175b3ee8dc1ecbaf9c45b86195c6
Cauã Will Sacramento <cauawillqssacramento@gmail.com>
2024-10-21 09:20:37 -0300

    Subindo utils.c/h com adições de funções para string

--- libs/utils.c
+++ libs/utils.c
@@ -13,7 +13,7 @@ void esperarEnter() {
     printf("\nPressione ENTER para continuar...\n");
     do {
         ch = getchar();
-    } while(ch != '\n' && ch != EOF);
+    } while(ch != '\n' && ch != EOF); // Isso é literalmente um "limparBuffer". Isso foi retirado do chat gpt.
 }
 
 void limparTela() {
@@ -24,4 +24,24 @@ void entradaInvalida() {
     limparTela();
     printf("Entrada inválida.");
     esperarEnter();
+}
+
+void toUpper(char txt[]) {
+    for(int iCont = 0; txt[iCont]; iCont++)
+        if(txt[iCont] >= 'a' && txt[iCont] <= 'z')
+            txt[iCont] = txt[iCont] - 'a' + 'A';
+}
+
+void toLower(char txt[]) {
+    for(int iCont = 0; txt[iCont]; iCont++)
+        if(txt[iCont] >= 'A' && txt[iCont] <= 'Z')
+            txt[iCont] = txt[iCont] - 'A' + 'a';
+}
+
+void copyStr(char txt1[], char txt2[]) {
+    int iCont;
+    for(iCont = 0; txt1[iCont]; iCont++) {
+        txt2[iCont] = txt1[iCont];
+    }
+    txt2[iCont] = '\0';
 }
--- libs/utils.h
+++ libs/utils.h
@@ -5,5 +5,7 @@ void limparBuffer();
 void esperarEnter();
 void limparTela();
 void entradaInvalida();
-
+void toUpper(char txt[]);
+void toLower(char txt[]);
+void copyStr(char txt1[], char txt2[]);
 #endif
