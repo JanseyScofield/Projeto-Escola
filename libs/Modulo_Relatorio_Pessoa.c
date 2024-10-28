06c6ae79a3b1a15fa08b6c7a937655fc8a3b66be
Cauã Will Sacramento <cauawillqssacramento@gmail.com>
2024-10-27 21:06:08 -0300

    Subindo módulo de relatório de pessoas com sua estrutura inicial.

---
+++ libs/Modulo_Relatorio_Pessoa.c
@@ -0,0 +1,82 @@
+#include "Modulo_Relatorio_Pessoa.h"
+#include "utils.h"
+void menuRelatorio(char tipo[]) {
+    char txt[20];
+    copyStr(tipo, txt);
+    toLower(txt);
+    printf("MODULO RELATORIO DE %s\n", tipo);
+    printf("0 - Voltar\n");
+    printf("1 - Listar %s\n", txt);
+    printf("2 - Listar %s por sexo\n", txt);
+    printf("3 - Listar %s por nome\n", txt);
+    printf("4 - Listar %s por data de nascimento\n", txt);
+    printf("5 - Procurar %s\n", txt);
+    if(tipo[0] == 'A')
+        printf("6 - Listar alunos matriculados em menos de 3 disciplinas\n");
+}
+
+
+void moduloRelatorioPessoa(Pessoa lstPessoa[], int *qtdPessoa, char tipo[]) {
+    int sair = 0;
+    char titulo[12];
+
+    if(tipo[0] == 'A')
+    {
+        char txt[12] = "Alunos";
+        copyStr(txt, titulo);
+    }
+    else 
+    {
+        char txt[12] = "Professores";
+        copyStr(txt, titulo);
+    }
+    
+    limparTela();
+
+    while(!sair)
+    {
+        int opcao;
+        limparTela();
+        menuRelatorio(tipo);
+
+        scanf("%d", &opcao);
+        limparBuffer();
+        
+        switch(opcao)
+        {
+            case 0: {
+                sair = 1;
+                break;
+            }
+            case 1: {
+                break;
+            }
+            case 2: {
+                break;
+            }
+            case 3: {
+                break;
+            }
+            case 4: {
+                break;
+            }
+            case 5: {
+                break;
+            }
+            case 6: {
+                if(tipo[0] == 'A')
+                {
+                    limparTela();
+                    printf("ALUNO!!");
+                    esperarEnter();
+                    break;
+                }
+            }
+            default: {
+                entradaInvalida();
+            }
+        }
+    }
+
+
+}
---
+++ libs/Modulo_Relatorio_Pessoa.h
@@ -0,0 +1,9 @@
+#include "pessoa/Pessoa.h"
+#ifndef MODULO_RELATORIO_PESSOA_H
+#define MODULO_RELATORIO_PESSOA_H
+
+void moduloRelatorioPessoa(Pessoa lstPessoa[], int *qtdPessoa, char tipo[]);
+void menuRelatorio(char tipo[]);
+
+
+#endif
