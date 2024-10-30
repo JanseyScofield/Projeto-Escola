fb8dc965f2eb00de96c21c0737befa31bd167f34
CwqSS <168303452+CwqSS@users.noreply.github.com>
2024-10-30 19:46:39 -0300

    Update Pessoa.h

--- libs/pessoa/Pessoa.h
+++ libs/pessoa/Pessoa.h
@@ -1,7 +1,6 @@
 #ifndef PESSOA
 #define PESSOA
 #define tam 256
-#define TAM_LISTAS 10
 #define TAM_CPF 12
 #include "../Data.h"
 
@@ -16,8 +15,8 @@ typedef struct {
 } Pessoa;
 
 int validarSexo(char sexo);
-int validarNumerosVerificadores(char cpf[TAM_CPF], int limite);
-int validarCPF(char cpf[TAM_CPF]);
+int validarNumerosVerificadores(char cpf[], int limite);
+int validarCPF(char cpf[]);
 int buscarPessoaPorCPF(Pessoa pessoas[], int qtdPessoas, char cpf[12]);
 void mostrarPessoa(Pessoa pessoa);
 void mostrarPessoas(Pessoa *pessoas, int qtdPessoas);
