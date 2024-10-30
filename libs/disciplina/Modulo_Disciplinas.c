6ae8be1cec4ad6a9543a064bdf99c3c68d52f73d
Jansey Scofield <janscofield10@gmail.com>
2024-10-30 11:11:49 -0300

    Atualizando modulo disciplina

--- libs/disciplina/Modulo_Disciplinas.c
+++ libs/disciplina/Modulo_Disciplinas.c
@@ -6,16 +6,15 @@ void menuDisciplinas(){
     printf("1 - Cadastrar Disciplina");
     printf("2 - Atualizar Disciplina\n");
     printf("3 - Excluir Disciplina\n");
-    printf("4 - Matricular aluno em uma disciplina\n");
-    printf("5 - Relatorios Disciplina\n");
+    printf("4 - Relatorios Disciplina\n");
 }
 void ModuloDisciplinas(Disciplina disciplinas[], int *qtdDisciplinas, Pessoa professores[], int qtdProfessores, Pessoa alunos[], int qtdAlunos){
     int sair = 0;
 
     while(!sair)
     {   
-        char entrada;
-        int opcao;
+        char entrada, entradaAtualizar;
+        int opcao,  opcaoAtualizar;
         
         limparTela();
         menuDisciplinas();
@@ -30,23 +29,47 @@ void ModuloDisciplinas(Disciplina disciplinas[], int *qtdDisciplinas, Pessoa pro
             }
             case 1: {
                 cadastrarDisciplina(disciplinas, qtdDisciplinas, professores,  qtdProfessores);
+                limparBuffer();
                 break;
             }
             case 2: {
-                // Em breve
+                printf("O que deseja atualizar?\n");
+                printf("0 - Voltar\n");
+                printf("1 - Dados da Disciplina");
+                printf("2 - Matricular um aluno\n");
+                printf("3 - Excluir um aluno\n");
+
+                scanf(" %c", &entradaAtualizar);
+                limparBuffer();
+                opcaoAtualizar = charToNumber(entradaAtualizar);
+                switch(opcaoAtualizar){
+                    case 0:
+                        break;
+                    case 1:
+                        // em breve
+                        break;
+                    case 2:
+                        matricularAluno(disciplinas, qtdDisciplinas, alunos, qtdAlunos);
+                        limparBuffer();
+                        break;
+                    case 3:
+                        excluirAlunoDisciplina(disciplinas, qtdDisciplinas);
+                        limparBuffer();
+                        break;
+                    default:
+                        entradaInvalida();
+                }   
                 break;
             }
             case 3: {
                 deletarDisciplina(disciplinas, qtdDisciplinas);
+                limparBuffer();
                 break;
             }
             case 4: {
-                matricularAluno(disciplinas, qtdDisciplinas, alunos,  qtdAlunos);
+                // em breve
                 break;
             }
-            case 5:
-                // Em breve
-                break;
             default: {
                 entradaInvalida();
             }
