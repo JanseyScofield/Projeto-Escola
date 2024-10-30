602d277f14eff40e725782d362896871ea3a4abe
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-10-30 20:20:35 -0300

    Corrigindo erros de parâmetros

--- libs/disciplina/Disciplinadb.h
+++ libs/disciplina/Disciplinadb.h
@@ -5,7 +5,7 @@
 
 void cadastrarDisciplina(Disciplina *disciplinas, int *qtdDisciplinas, Pessoa *professores, int qtdProfessores);
 void deletarDisciplina(Disciplina *disciplinas, int qtdDisciplinas);
-void atualizarDisciplina(Disciplina *disciplinas, int qtdDisciplinas);
+void atualizarDisciplina(Disciplina *disciplinas, int qtdDisciplinas, Pessoa *professores, int qtdProfessores);
 void matricularAluno(Disciplina *disciplinas, int qtdDisciplinas, Pessoa *alunos, int qtdAlunos);
 void excluirAlunoDisciplina(Disciplina *disciplinas, int qtdDisciplinas);
 #endif
